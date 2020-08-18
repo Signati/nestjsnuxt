2df60dbb35be8fc5835e79c565e18ef0ad8c3fab
MisaelMa <amisael.amir.misael@gmail.com>
2020-08-18 15:52:42 -0500

    se añadio los tipos de vue

--- client/pages/about/index.vue
+++ client/pages/about/index.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
-    <h1 ref="titleRef">TypeScript Playground</h1>
-    <button type="button" @click="onClickChangeStatusBtn">Change Status</button>
-    <StatusChecker :status="status" />
-  </div>
+	<div>
+		<h1 ref="titleRef">TypeScript Playground</h1>
+		<v-btn color="primary" dark @click="onClickChangeStatusBtn">Change Status</v-btn>
+		<StatusChecker :status="status"/>
+	</div>
 </template>
 
 <script lang="ts">
-import { ref, toRefs, reactive, onMounted } from '@vue/composition-api'
-import useChangeStatus from '../../hooks/useChangeStatus'
-import StatusChecker from '../../components/StatusChecker.vue'
+import { ref, toRefs, reactive, onMounted } from '@vue/composition-api';
+import useChangeStatus from '../../hooks/useChangeStatus';
+import StatusChecker from '../../components/StatusChecker.vue';
 
 export default {
   components: {
@@ -20,27 +20,27 @@ export default {
     // NOTE: reactive with Array
     const unwrapTestState = reactive({
       list: [reactive({ count: ref(0) })],
-    })
-    const newItem = reactive({ count: ref(10) })
-    const newItem2 = reactive({ count: 10 })
-    unwrapTestState.list.push(newItem)
-    unwrapTestState.list.push(newItem2)
-    console.log(unwrapTestState.list[0].count === 0) // Unwrap される（value は不要）
-    console.log(unwrapTestState.list[1].count === 10)
-    console.log(unwrapTestState.list[2].count === 10)
+    });
+    const newItem = reactive({ count: ref(10) });
+    const newItem2 = reactive({ count: 10 });
+    unwrapTestState.list.push(newItem);
+    unwrapTestState.list.push(newItem2);
+    console.log(unwrapTestState.list[0].count === 0); // Unwrap される（value は不要）
+    console.log(unwrapTestState.list[1].count === 10);
+    console.log(unwrapTestState.list[2].count === 10);
 
     // NOTE: template ref
-    const titleRef = ref(null)
-    const { status, toggleStatus } = useChangeStatus()
+    const titleRef = ref(null);
+    const { status, toggleStatus } = useChangeStatus();
 
     const onClickChangeStatusBtn = () => {
-      toggleStatus()
-    }
+      toggleStatus();
+    };
 
     onMounted(() => {
-      const el: any = titleRef.value
-      el.style.color = '#338'
-    })
+      const el: any = titleRef.value;
+      el.style.color = '#338';
+    });
 
     return {
       ...toRefs({
@@ -48,7 +48,7 @@ export default {
       }),
       onClickChangeStatusBtn,
       titleRef,
-    }
+    };
   },
-}
+};
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 import { Configuration } from '@nuxt/types/config';
 import { colors } from 'vuetify/lib';
 
+const isDev = !(process.env.NODE_ENV === 'production');
+
 const config: Configuration = {
   telemetry: false,
   srcDir: 'client/',
@@ -8,10 +10,27 @@ const config: Configuration = {
     port: 8080,
   },
   mode: 'universal',
-  /*
-  ** Headers of the page
-  */
+  dev: isDev,
 
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: 'title',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'description' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons',
+      },
+    ],
+  },
   /*
   ** Customize the progress-bar color
   */
